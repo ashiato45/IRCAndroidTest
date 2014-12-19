cc58de5cbe3b89fba36839441cb1b0cb06a67ed5
ashiato45 <katiiijp@gmail.com>
2014-12-19 15:54:37 +0900

    Input and hiding software keyboard completed.

--- src/com/ashiato45/test2/MainActivity.java
+++ src/com/ashiato45/test2/MainActivity.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity{
@@ -35,17 +40,50 @@ public class MainActivity extends ActionBarActivity{
 		t.setText("This is Tom.");*/
 
 		Button b = (Button)findViewById(R.id.mainButton);
+		final TextView inputText = (TextView)findViewById(R.id.mainEdit);
+		final TextView scroll = (TextView)findViewById(R.id.mainScroll2);
+		final RelativeLayout rl = (RelativeLayout)findViewById(R.id.mainLayout);
+
 		b.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO 自動生成されたメソッド・スタブ
-				TextView inputText = (TextView)findViewById(R.id.mainEdit);
-				TextView scroll = (TextView)findViewById(R.id.mainScroll2);
-				scroll.setText(inputText.getText());
+
+				addAndClear();
 			}
 		});
 
+		inputText.setOnKeyListener(new OnKeyListener() {
+
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// TODO 自動生成されたメソッド・スタブ
+				if(event.getAction() == KeyEvent.ACTION_DOWN
+						&& keyCode == KeyEvent.KEYCODE_ENTER){
+					InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+					addAndClear();
+				}
+				return false;
+			}
+		});
+
+	}
+
+	protected void addAndClear(){
+		TextView inputText = (TextView)findViewById(R.id.mainEdit);
+		addScroll(inputText.getText().toString());
+		inputText.setText("");
+	}
+
+	protected void addScroll(String message) {
+		TextView scroll = (TextView)findViewById(R.id.mainScroll2);
+
+		String current = scroll.getText().toString();
+		current += "\n";
+		current += message;
+		scroll.setText(current);
 	}
 
 	@Override
